acb39d844e819b4a1104e0819ef29654c8a17749
Daniel Cos <alexcos_14@yahoo.es>
2017-02-12 19:01:27 -0600

    Modificaciones 12/02/2017 19:01

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -15,5 +15,8 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola Mundo");               
+        
     }
 }
